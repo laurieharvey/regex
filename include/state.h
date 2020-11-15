d948e7c17a075b0728b63baba7049b3a413ccbcf
laurie <laurieharvey1602@gmail.com>
2020-11-15 20:30:58 +0000

    Format state.h

--- include/state.h
+++ include/state.h
@@ -7,43 +7,49 @@
 
 namespace fa
 {
-	enum class match { accepted, rejected };
+	enum class match
+	{
+		accepted,
+		rejected
+	};
 
-	template<typename CharT>
+	template <typename CharT>
 	class state
 	{
 	public:
-		enum class accepting { accepting, nonaccepting };
-
-		state( accepting acc = accepting::nonaccepting )
-			: acc_( acc )
-			, transitionsMap_( )
+		enum class accepting
 		{
+			accepting,
+			nonaccepting
+		};
 
+		state(accepting acc = accepting::nonaccepting)
+			: acc_(acc), transitionsMap_()
+		{
 		}
 
-		void set( accepting acc )
+		void set(accepting acc)
 		{
 			acc_ = acc;
 		}
 
-		void connect( CharT symbol, std::shared_ptr<state<CharT>> st )
+		void connect(CharT symbol, std::shared_ptr<state<CharT>> st)
 		{
-			transitionsMap_[ symbol ].push_back( st );
+			transitionsMap_[symbol].push_back(st);
 		}
 
-		match next( std::basic_string_view<CharT> str )
+		match next(std::basic_string_view<CharT> str)
 		{
-			if( str.empty( ) )
+			if (str.empty())
 			{
-				if( acc_ == accepting::accepting )
+				if (acc_ == accepting::accepting)
 				{
 					return match::accepted;
 				}
 
-				for( std::shared_ptr<state<CharT>>& next : transitionsMap_[ 0x01 ] )
+				for (std::shared_ptr<state<CharT>> &next : transitionsMap_[0x01])
 				{
-					if( next->next( str ) == match::accepted )
+					if (next->next(str) == match::accepted)
 					{
 						return match::accepted;
 					}
@@ -52,17 +58,17 @@ namespace fa
 				return match::rejected;
 			}
 
-			for( std::shared_ptr<state<CharT>>& next : transitionsMap_[ str[ 0 ] ] )
+			for (std::shared_ptr<state<CharT>> &next : transitionsMap_[str[0]])
 			{
-				if( next->next( str.substr( 1 ) ) == match::accepted )
+				if (next->next(str.substr(1)) == match::accepted)
 				{
 					return match::accepted;
 				}
 			}
 
-			for( std::shared_ptr<state<CharT>>& next : transitionsMap_[ 0x01 ] )
+			for (std::shared_ptr<state<CharT>> &next : transitionsMap_[0x01])
 			{
-				if( next->next( str ) == match::accepted )
+				if (next->next(str) == match::accepted)
 				{
 					return match::accepted;
 				}
@@ -70,8 +76,9 @@ namespace fa
 
 			return match::rejected;
 		}
+
 	private:
 		accepting acc_;
 		std::map<CharT, std::vector<std::shared_ptr<state<CharT>>>> transitionsMap_;
 	};
-}
+} // namespace fa
