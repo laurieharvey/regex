87344f39534210deb9b81a4f0f427ce10fb40e92
laurie <laurieharvey1602@gmail.com>
2020-11-15 16:21:08 +0000

    Add tests for nfa

---
+++ test/test_nfa.cpp
@@ -0,0 +1,87 @@
+#include <sstream>
+#include <gtest/gtest.h>
+
+#include "nfa.h"
+
+TEST(nfa_test, character)
+{
+	EXPECT_EQ(fa::nfa<char>::from_character('a')->run("a"),
+			  fa::match::accepted);
+
+	EXPECT_EQ(fa::nfa<char>::from_character('a')->run("b"),
+			  fa::match::rejected);
+}
+
+TEST(nfa_test, epsilon)
+{
+	EXPECT_EQ(fa::nfa<char>::from_epsilon()->run("a"),
+			  fa::match::rejected);
+}
+
+TEST(nfa_test, concatenation)
+{
+	EXPECT_EQ(fa::nfa<char>::from_concatenation(fa::nfa<char>::from_character('a'), fa::nfa<char>::from_character('b'))->run("ab"),
+			  fa::match::accepted);
+
+	EXPECT_EQ(fa::nfa<char>::from_concatenation(fa::nfa<char>::from_character('a'), fa::nfa<char>::from_character('b'))->run("ac"),
+			  fa::match::rejected);
+}
+
+TEST(nfa_test, alternation)
+{
+	EXPECT_EQ(fa::nfa<char>::from_alternation(fa::nfa<char>::from_character('a'), fa::nfa<char>::from_character('b'))->run("a"),
+			  fa::match::accepted);
+
+	EXPECT_EQ(fa::nfa<char>::from_alternation(fa::nfa<char>::from_character('a'), fa::nfa<char>::from_character('b'))->run("c"),
+			  fa::match::rejected);
+}
+
+TEST(nfa_test, kleene)
+{
+	EXPECT_EQ(fa::nfa<char>::from_kleene(fa::nfa<char>::from_character('a'))->run(""),
+			  fa::match::accepted);
+
+	EXPECT_EQ(fa::nfa<char>::from_kleene(fa::nfa<char>::from_character('a'))->run("a"),
+			  fa::match::accepted);
+
+	EXPECT_EQ(fa::nfa<char>::from_kleene(fa::nfa<char>::from_character('a'))->run("aa"),
+			  fa::match::accepted);
+
+	EXPECT_EQ(fa::nfa<char>::from_kleene(fa::nfa<char>::from_character('a'))->run("b"),
+			  fa::match::rejected);
+
+	EXPECT_EQ(fa::nfa<char>::from_kleene(fa::nfa<char>::from_character('a'))->run("ab"),
+			  fa::match::rejected);
+
+	EXPECT_EQ(fa::nfa<char>::from_kleene(fa::nfa<char>::from_character('a'))->run("ba"),
+			  fa::match::rejected);
+}
+
+TEST(nfa_test, complex)
+{
+	auto state_machine = fa::nfa<char>::from_alternation(
+		fa::nfa<char>::from_concatenation(
+			fa::nfa<char>::from_concatenation(
+				fa::nfa<char>::from_character('a'),
+				fa::nfa<char>::from_kleene(fa::nfa<char>::from_character('b'))),
+			fa::nfa<char>::from_character('c')),
+		fa::nfa<char>::from_kleene(fa::nfa<char>::from_character('d')));
+
+	EXPECT_EQ(state_machine->run("abbc"),
+			  fa::match::accepted);
+
+	EXPECT_EQ(state_machine->run("ac"),
+			  fa::match::accepted);
+
+	EXPECT_EQ(state_machine->run(""),
+			  fa::match::accepted);
+
+	EXPECT_EQ(state_machine->run("dd"),
+			  fa::match::accepted);
+
+	EXPECT_EQ(state_machine->run("e"),
+			  fa::match::rejected);
+
+	EXPECT_EQ(state_machine->run("bc"),
+			  fa::match::rejected);
+}
