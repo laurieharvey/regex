517b0a6e61f12d8344453b8569a37602bf983a16
laurie <laurieharvey1602@gmail.com>
2020-11-23 20:22:20 +0000

    Add main executable

---
+++ src/regex.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <functional>
+#include <sstream>
+
+#include "parser.h"
+#include "nfa.h"
+
+int main()
+{
+  std::stringstream regex = std::stringstream( "ab*|c+" );
+  regex = ast::make_explicit( regex );
+
+  auto ast = ast::parse(regex);
+
+  fa::generator<char> g;
+
+  ast->print(std::cout);
+  ast->walk(std::bind(&fa::generator<char>::callback, g, std::placeholders::_1));
+  auto n = g.result();
+
+  auto result = n->run("c");
+
+  if( result == fa::match::accepted )
+    std::cout << "Accepted" << std::endl;
+  else
+  {
+    std::cout << "Rejected" << std::endl;
+  }
+  
+
+  return 0;
+}
