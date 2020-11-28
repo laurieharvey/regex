66ee82827db163aa5e05b00d19e40b6595382dd5
laurie <laurieharvey1602@gmail.com>
2020-11-28 12:48:18 +0000

    Tidy tests

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -8,39 +8,47 @@ TEST( make_explicit_test, alternation )
 	std::stringstream implicit_regex( "a|b" );
 	std::stringstream explicit_regex = ast::make_explicit( implicit_regex );
 
-	EXPECT_EQ( explicit_regex.str(), std::string( "a|b" ) );
+	EXPECT_EQ( explicit_regex.str(), implicit_regex.str() );
 }
 
 TEST( make_explicit_test, question )
 {
-	std::stringstream implicit_regex( "a?b" );
+	std::stringstream implicit_regex( "a?" );
 	std::stringstream explicit_regex = ast::make_explicit( implicit_regex );
 
-	EXPECT_EQ( explicit_regex.str( ), std::string( "a?.b" ) );
+	EXPECT_EQ( explicit_regex.str( ), implicit_regex.str() );
 }
 
 TEST( make_explicit_test, kleene )
 {
-	std::stringstream implicit_regex( "a*b" );
+	std::stringstream implicit_regex( "a*" );
 	std::stringstream explicit_regex = ast::make_explicit( implicit_regex );
 
-	EXPECT_EQ( explicit_regex.str( ), std::string( "a*.b" ) );
+	EXPECT_EQ( explicit_regex.str( ), implicit_regex.str() );
+}
+
+TEST( make_explicit_test, one_or_more )
+{
+	std::stringstream implicit_regex( "a+" );
+	std::stringstream explicit_regex = ast::make_explicit( implicit_regex );
+
+	EXPECT_EQ( explicit_regex.str( ), implicit_regex.str() );
 }
 
 TEST( make_explicit_test, parenthesis )
 {
-	std::stringstream implicit_regex( "a(c)b" );
+	std::stringstream implicit_regex( "(c)" );
 	std::stringstream explicit_regex = ast::make_explicit( implicit_regex );
 
-	EXPECT_EQ( explicit_regex.str( ), std::string( "a.(c).b" ) );
+	EXPECT_EQ( explicit_regex.str( ), implicit_regex.str() );
 }
 
 TEST( make_explicit_test, complex )
 {
-	std::stringstream implicit_regex( "a?(c*|d)b*e" );
+	std::stringstream implicit_regex( "a?(c*|d+)b*e" );
 	std::stringstream explicit_regex = ast::make_explicit( implicit_regex );
 
-	EXPECT_EQ( explicit_regex.str( ), std::string( "a?.(c*|d).b*.e" ) );
+	EXPECT_EQ( explicit_regex.str( ), std::string( "a?.(c*|d+).b*.e" ) );
 }
 
 TEST( parse_test, alternation )
@@ -55,40 +63,50 @@ TEST( parse_test, alternation )
 
 TEST( parse_test, question )
 {
-	std::stringstream input( "a?.b" );
+	std::stringstream input( "a?" );
 	std::stringstream output;
 
 	ast::parse( input )->print( output );
 
-	EXPECT_EQ( output.str( ), std::string( "a?b" ) );
+	EXPECT_EQ( output.str( ), input.str() );
 }
 
 TEST( parse_test, kleene )
 {
-	std::stringstream input( "a*.b" );
+	std::stringstream input( "a*" );
 	std::stringstream output;
 
 	ast::parse( input )->print( output );
 
-	EXPECT_EQ( output.str( ), std::string( "a*b" ) );
+	EXPECT_EQ( output.str( ), input.str() );
+}
+
+TEST( parse_test, one_or_more )
+{
+	std::stringstream input( "a+" );
+	std::stringstream output;
+
+	ast::parse( input )->print( output );
+
+	EXPECT_EQ( output.str( ), input.str() );
 }
 
 TEST( parse_test, parenthesis )
 {
-	std::stringstream input( "a.(c).b" );
+	std::stringstream input( "(c)" );
 	std::stringstream output;
 
 	ast::parse( input )->print( output );
 
-	EXPECT_EQ( output.str( ), std::string( "a(c)b" ) );
+	EXPECT_EQ( output.str( ), input.str() );
 }
 
 TEST( parse_test, complex )
 {
-	std::stringstream input( "a?.(c*|d).b*.e" );
+	std::stringstream input( "a?.(c*|d+).b*.e" );
 	std::stringstream output;
 
 	ast::parse( input )->print( output );
 
-	EXPECT_EQ( output.str( ), std::string( "a?(c*|d)b*e" ) );
+	EXPECT_EQ( output.str( ), std::string( "a?(c*|d+)b*e" ) );
 }
