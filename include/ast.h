04bba5bc5c6259859ac06040a65b866c9088abf2
laurie <laurieharvey1602@gmail.com>
2020-12-26 14:21:32 +0000

    Format

--- include/ast.h
+++ include/ast.h
@@ -8,272 +8,289 @@
 
 namespace ast
 {
-	template<typename CharT>
-	constexpr std::vector<CharT> get_operators( )
+	template <typename CharT>
+	constexpr std::vector<CharT> get_operators()
 	{
-		return { '(', ')', '*', '?', '+', '.', '|' };
+		return {'(', ')', '*', '?', '+', '.', '|'};
 	}
 
-	template<typename CharT>
-	constexpr std::vector<CharT> get_alphabet( )
+	template <typename CharT>
+	constexpr std::vector<CharT> get_alphabet()
 	{
-		std::vector<CharT> alphabet, operators = get_operators<CharT>();		
+		std::vector<CharT> alphabet, operators = get_operators<CharT>();
 
-		for( CharT i = 0; i < std::numeric_limits<CharT>::max(); ++i )
+		for (CharT i = 0; i < std::numeric_limits<CharT>::max(); ++i)
 		{
-			if( std::find( std::cbegin( operators ), std::cend( operators), i ) == std::cend( operators ) )
+			if (std::find(std::cbegin(operators), std::cend(operators), i) == std::cend(operators))
 			{
-				alphabet.push_back( i );
+				alphabet.push_back(i);
 			}
 		}
 
 		return alphabet;
 	}
 
-	enum class type { character, parenthesis, kleene, zero_or_one, one_or_more, alternation, concatenation, any };
+	enum class type
+	{
+		character,
+		parenthesis,
+		kleene,
+		zero_or_one,
+		one_or_more,
+		alternation,
+		concatenation,
+		any
+	};
 
-	template<typename CharT>
+	template <typename CharT>
 	class token
 	{
 	public:
 		using ostream = std::basic_ostream<CharT, std::char_traits<CharT>>;
 
-		virtual void print( ostream& os ) const = 0;
+		virtual void print(ostream &os) const = 0;
 
-		virtual void walk( std::function<void( const ast::token<CharT>& )> callback ) const = 0;
+		virtual void walk(std::function<void(const ast::token<CharT> &)> callback) const = 0;
 
-		virtual CharT get_token( ) const = 0;
+		virtual CharT get_token() const = 0;
 
-		virtual type get_type( ) const = 0;
+		virtual type get_type() const = 0;
 	};
 
-	template<typename CharT>
+	template <typename CharT>
 	class character : public token<CharT>
 	{
 		CharT value_;
-	public:
-		explicit character( CharT value )
-			: value_( value )
-		{
 
-		}
-		void print( typename token<CharT>::ostream& os ) const override
+	public:
+		explicit character(CharT value)
+			: value_(value)
 		{
-			os << get_token( );
 		}
-		void walk( std::function<void( const ast::token<CharT>& )> callback ) const override
+		void print(typename token<CharT>::ostream &os) const override
 		{
-			callback( *this );
+			os << get_token();
 		}
-		CharT get_token( ) const override
+		void walk(std::function<void(const ast::token<CharT> &)> callback) const override
+		{
+			callback(*this);
+		}
+		CharT get_token() const override
 		{
 			return value_;
 		}
-		type get_type( ) const override
+		type get_type() const override
 		{
 			return type::character;
 		}
 	};
 
-	template<typename CharT>
+	template <typename CharT>
 	class parenthesis : public token<CharT>
 	{
 		std::unique_ptr<token<CharT>> lhs_;
-	public:
-		explicit parenthesis( std::unique_ptr<token<CharT>> lhs )
-			: lhs_( std::move( lhs ) )
-		{
 
-		}
-		void print( typename token<CharT>::ostream& os ) const override
+	public:
+		explicit parenthesis(std::unique_ptr<token<CharT>> lhs)
+			: lhs_(std::move(lhs))
 		{
-			os << '('; lhs_->print( os ); os << ")";
 		}
-		void walk( std::function<void( const ast::token<CharT>& )> callback ) const override
+		void print(typename token<CharT>::ostream &os) const override
 		{
-			lhs_->walk( callback );
-			callback( *this );
+			os << '(';
+			lhs_->print(os);
+			os << ")";
 		}
-		CharT get_token( ) const override
+		void walk(std::function<void(const ast::token<CharT> &)> callback) const override
+		{
+			lhs_->walk(callback);
+			callback(*this);
+		}
+		CharT get_token() const override
 		{
 			return 0;
 		}
-		type get_type( ) const override
+		type get_type() const override
 		{
 			return type::parenthesis;
 		}
 	};
 
-	template<typename CharT>
+	template <typename CharT>
 	class kleene : public token<CharT>
 	{
 		std::unique_ptr<token<CharT>> lhs_;
-	public:
-		explicit kleene( std::unique_ptr<token<CharT>> lhs )
-			: lhs_( std::move( lhs ) )
-		{
 
-		}
-		void print( typename token<CharT>::ostream& os ) const override
+	public:
+		explicit kleene(std::unique_ptr<token<CharT>> lhs)
+			: lhs_(std::move(lhs))
 		{
-			lhs_->print( os ); os << '*';
 		}
-		void walk( std::function<void( const ast::token<CharT>& )> callback ) const override
+		void print(typename token<CharT>::ostream &os) const override
 		{
-			lhs_->walk( callback );
-			callback( *this );
+			lhs_->print(os);
+			os << '*';
 		}
-		CharT get_token( ) const override
+		void walk(std::function<void(const ast::token<CharT> &)> callback) const override
+		{
+			lhs_->walk(callback);
+			callback(*this);
+		}
+		CharT get_token() const override
 		{
 			return '*';
 		}
-		type get_type( ) const override
+		type get_type() const override
 		{
 			return type::kleene;
 		}
 	};
 
-	template<typename CharT>
+	template <typename CharT>
 	class zero_or_one : public token<CharT>
 	{
 		std::unique_ptr<token<CharT>> lhs_;
-	public:
-		explicit zero_or_one( std::unique_ptr<token<CharT>> lhs )
-			: lhs_( std::move( lhs ) )
-		{
 
-		}
-		void print( typename token<CharT>::ostream& os ) const override
+	public:
+		explicit zero_or_one(std::unique_ptr<token<CharT>> lhs)
+			: lhs_(std::move(lhs))
 		{
-			lhs_->print( os ); os << '?';
 		}
-		void walk( std::function<void( const ast::token<CharT>& )> callback ) const override
+		void print(typename token<CharT>::ostream &os) const override
 		{
-			lhs_->walk( callback );
-			callback( *this );
+			lhs_->print(os);
+			os << '?';
 		}
-		CharT get_token( ) const override
+		void walk(std::function<void(const ast::token<CharT> &)> callback) const override
+		{
+			lhs_->walk(callback);
+			callback(*this);
+		}
+		CharT get_token() const override
 		{
 			return '?';
 		}
-		type get_type( ) const override
+		type get_type() const override
 		{
 			return type::zero_or_one;
 		}
 	};
 
-	template<typename CharT>
+	template <typename CharT>
 	class one_or_more : public token<CharT>
 	{
 		std::unique_ptr<token<CharT>> exp_;
-	public:
-		explicit one_or_more( std::unique_ptr<token<CharT>> exp )
-			: exp_( std::move( exp ) )
-		{
 
-		}
-		void print( typename token<CharT>::ostream& os ) const override
+	public:
+		explicit one_or_more(std::unique_ptr<token<CharT>> exp)
+			: exp_(std::move(exp))
 		{
-			exp_->print( os ); os << get_token( );
 		}
-		void walk( std::function<void( const ast::token<CharT>& )> callback ) const override
+		void print(typename token<CharT>::ostream &os) const override
 		{
-			exp_->walk( callback );
-			callback( *this );
+			exp_->print(os);
+			os << get_token();
 		}
-		CharT get_token( ) const override
+		void walk(std::function<void(const ast::token<CharT> &)> callback) const override
+		{
+			exp_->walk(callback);
+			callback(*this);
+		}
+		CharT get_token() const override
 		{
 			return '+';
 		}
-		type get_type( ) const override
+		type get_type() const override
 		{
 			return type::one_or_more;
 		}
 	};
 
-	template<typename CharT>
+	template <typename CharT>
 	class alternation : public token<CharT>
 	{
 		std::unique_ptr<token<CharT>> lhs_, rhs_;
-	public:
-		explicit alternation( std::unique_ptr<token<CharT>> lhs, std::unique_ptr<token<CharT>> rhs )
-			: lhs_( std::move( lhs ) ), rhs_( std::move( rhs ) )
-		{
 
-		}
-		void print( typename token<CharT>::ostream& os ) const override
+	public:
+		explicit alternation(std::unique_ptr<token<CharT>> lhs, std::unique_ptr<token<CharT>> rhs)
+			: lhs_(std::move(lhs)), rhs_(std::move(rhs))
 		{
-			lhs_->print( os ); os << '|'; rhs_->print( os );
 		}
-		void walk( std::function<void( const ast::token<CharT>& )> callback ) const override
+		void print(typename token<CharT>::ostream &os) const override
 		{
-			lhs_->walk( callback );
-			rhs_->walk( callback );
-			callback( *this );
+			lhs_->print(os);
+			os << '|';
+			rhs_->print(os);
 		}
-		CharT get_token( ) const override
+		void walk(std::function<void(const ast::token<CharT> &)> callback) const override
+		{
+			lhs_->walk(callback);
+			rhs_->walk(callback);
+			callback(*this);
+		}
+		CharT get_token() const override
 		{
 			return '|';
 		}
-		type get_type( ) const override
+		type get_type() const override
 		{
 			return type::alternation;
 		}
 	};
 
-	template<typename CharT>
+	template <typename CharT>
 	class concatenation : public token<CharT>
 	{
 		std::unique_ptr<token<CharT>> lhs_, rhs_;
-	public:
-		explicit concatenation( std::unique_ptr<token<CharT>> lhs, std::unique_ptr<token<CharT>> rhs )
-			: lhs_( std::move( lhs ) ), rhs_( std::move( rhs ) )
-		{
 
-		}
-		void print( typename token<CharT>::ostream& os ) const override
+	public:
+		explicit concatenation(std::unique_ptr<token<CharT>> lhs, std::unique_ptr<token<CharT>> rhs)
+			: lhs_(std::move(lhs)), rhs_(std::move(rhs))
 		{
-			lhs_->print( os ); rhs_->print( os );
 		}
-		void walk( std::function<void( const ast::token<CharT>& )> callback ) const override
+		void print(typename token<CharT>::ostream &os) const override
 		{
-			lhs_->walk( callback );
-			rhs_->walk( callback );
-			callback( *this );
+			lhs_->print(os);
+			rhs_->print(os);
 		}
-		CharT get_token( ) const override
+		void walk(std::function<void(const ast::token<CharT> &)> callback) const override
+		{
+			lhs_->walk(callback);
+			rhs_->walk(callback);
+			callback(*this);
+		}
+		CharT get_token() const override
 		{
 			return '-';
 		}
-		type get_type( ) const override
+		type get_type() const override
 		{
 			return type::concatenation;
 		}
 	};
 
-	template<typename CharT>
+	template <typename CharT>
 	class any : public token<CharT>
 	{
 	public:
-		explicit any( )
+		explicit any()
 		{
-
 		}
-		void print( typename token<CharT>::ostream& os ) const override
+		void print(typename token<CharT>::ostream &os) const override
 		{
 			os << '.';
 		}
-		void walk( std::function<void( const ast::token<CharT>& )> callback ) const override
+		void walk(std::function<void(const ast::token<CharT> &)> callback) const override
 		{
-			callback( *this );
+			callback(*this);
 		}
-		CharT get_token( ) const override
+		CharT get_token() const override
 		{
 			return '.';
 		}
-		type get_type( ) const override
+		type get_type() const override
 		{
 			return type::any;
 		}
 	};
-}
+} // namespace ast
