f79f6bbbe63b1c8843f9b28c9433f0bb2b2b4030
laurie <laurieharvey1602@gmail.com>
2021-03-07 13:57:36 +0000

    Compile flags

--- include/compile.h
+++ include/compile.h
@@ -1,10 +1,18 @@
+#pragma once
+
 #include <sstream>
 #include <memory>
 
 #include "fa.h"
 #include "ast.h"
 
+enum compile_flag
+{
+    COMPILE_NFA = 0x01,
+    COMPILE_DFA = 0x02,
+};
+using compile_flags = std::underlying_type<compile_flag>::type;
 /*
  * Compile the regular expression to its finite automaton
  */
-std::shared_ptr<regex::fa> compile(std::basic_stringstream<regex::character_type>&& pattern);
+std::shared_ptr<regex::fa> compile(std::basic_stringstream<regex::character_type>&& pattern, compile_flags flags = COMPILE_NFA );
