cceab017e7864bcc53eaeafe1412b27dc698efaa
Laurie Harvey <laurieharvey1602@gmail.com>
2021-04-11 18:06:30 +0100

    Virtual destructor

--- include/automata/fa.h
+++ include/automata/fa.h
@@ -8,8 +8,14 @@ namespace regex
     class fa
     {
     public:
+        virtual ~fa( ){ };
+        /*
+         *  Recursively walk the automata passing each state to callback 
+         */
         virtual void walk( std::function<void( std::shared_ptr<state> )> callback ) = 0;
-
+        /*
+         *  Run target against the automata
+         */
         virtual match execute( std::basic_string_view<language::character_type> target ) = 0;
     };
 }
