5d4da86ae31be5dcfb9b2636ce0ce6e0119f90b9
laurie <laurieharvey1602@gmail.com>
2022-02-13 12:09:55 +0000

    Comments

--- include/language/ast.h
+++ include/language/ast.h
@@ -12,29 +12,27 @@ namespace regex
 {
     namespace language
     {
-        enum class type
-        {
-            literal,
-            any,
-            parenthesis,
-            kleene,
-            zero_or_one,
-            one_or_more,
-            alternation,
-            concatenation
-        };
+        enum class type { literal, any, parenthesis, kleene, zero_or_one, one_or_more, alternation, concatenation };
 
         class token
         {
           public:
             using ostream = std::basic_ostream<character_type, std::char_traits<character_type>>;
-
+            /*
+             * Output this token and its arguments to the output stream
+             */
             virtual void to_string( ostream &os ) const = 0;
-
+            /*
+             * Depth-first traversal of the AST
+             */
             virtual void walk( std::function<void( const token & )> callback ) const = 0;
-
+            /*
+             * Get the character representation of this token
+             */
             virtual character_type get_character() const = 0;
-
+            /*
+             * Get the type of this token
+             */
             virtual type get_type() const = 0;
 
             virtual ~token() = default;
@@ -51,13 +49,21 @@ namespace regex
             explicit literal( character_type value );
             explicit literal( const literal & ) = delete;
             explicit literal( literal && ) = delete;
-
+            /*
+             * Output this token and its arguments to the output stream
+             */
             void to_string( typename token::ostream &os ) const override;
-
+            /*
+             * Depth-first traversal of the AST
+             */
             void walk( std::function<void( const token & )> callback ) const override;
-
+            /*
+             * Get the character representation of this token
+             */
             character_type get_character() const override;
-
+            /*
+             * Get the type of this token
+             */
             type get_type() const override;
         };
 
@@ -70,13 +76,21 @@ namespace regex
             explicit any();
             explicit any( const any & ) = delete;
             explicit any( any && ) = delete;
-
+            /*
+             * Output this token and its arguments to the output stream
+             */
             void to_string( typename token::ostream &os ) const override;
-
+            /*
+             * Depth-first traversal of the AST
+             */
             void walk( std::function<void( const token & )> callback ) const override;
-
+            /*
+             * Get the character representation of this token
+             */
             character_type get_character() const override;
-
+            /*
+             * Get the type of this token
+             */
             type get_type() const override;
         };
 
@@ -92,13 +106,21 @@ namespace regex
             explicit parenthesis( std::unique_ptr<token> lhs );
             explicit parenthesis( const parenthesis & ) = delete;
             explicit parenthesis( parenthesis && ) = delete;
-
+            /*
+             * Output this token and its arguments to the output stream
+             */
             void to_string( typename token::ostream &os ) const override;
-
+            /*
+             * Depth-first traversal of the AST
+             */
             void walk( std::function<void( const token & )> callback ) const override;
-
+            /*
+             * Get the character representation of this token
+             */
             character_type get_character() const override;
-
+            /*
+             * Get the type of this token
+             */
             type get_type() const override;
         };
 
@@ -114,13 +136,21 @@ namespace regex
             explicit kleene( std::unique_ptr<token> lhs );
             explicit kleene( const kleene & ) = delete;
             explicit kleene( kleene && ) = delete;
-
+            /*
+             * Output this token and its arguments to the output stream
+             */
             void to_string( typename token::ostream &os ) const override;
-
+            /*
+             * Depth-first traversal of the AST
+             */
             void walk( std::function<void( const token & )> callback ) const override;
-
+            /*
+             * Get the character representation of this token
+             */
             character_type get_character() const override;
-
+            /*
+             * Get the type of this token
+             */
             type get_type() const override;
         };
 
@@ -136,13 +166,21 @@ namespace regex
             explicit zero_or_one( std::unique_ptr<token> lhs );
             explicit zero_or_one( const zero_or_one & ) = delete;
             explicit zero_or_one( zero_or_one && ) = delete;
-
+            /*
+             * Output this token and its arguments to the output stream
+             */
             void to_string( typename token::ostream &os ) const override;
-
+            /*
+             * Depth-first traversal of the AST
+             */
             void walk( std::function<void( const token & )> callback ) const override;
-
+            /*
+             * Get the character representation of this token
+             */
             character_type get_character() const override;
-
+            /*
+             * Get the type of this token
+             */
             type get_type() const override;
         };
 
@@ -158,13 +196,21 @@ namespace regex
             explicit one_or_more( std::unique_ptr<token> exp );
             explicit one_or_more( const one_or_more & ) = delete;
             explicit one_or_more( one_or_more && ) = delete;
-
+            /*
+             * Output this token and its arguments to the output stream
+             */
             void to_string( typename token::ostream &os ) const override;
-
+            /*
+             * Depth-first traversal of the AST
+             */
             void walk( std::function<void( const token & )> callback ) const override;
-
+            /*
+             * Get the character representation of this token
+             */
             character_type get_character() const override;
-
+            /*
+             * Get the type of this token
+             */
             type get_type() const override;
         };
 
@@ -180,13 +226,21 @@ namespace regex
             explicit alternation( std::unique_ptr<token> lhs, std::unique_ptr<token> rhs );
             explicit alternation( const alternation & ) = delete;
             explicit alternation( alternation && ) = delete;
-
-            void to_string( typename token::ostream & os ) const override;
-
+            /*
+             * Output this token and its arguments to the output stream
+             */
+            void to_string( typename token::ostream &os ) const override;
+            /*
+             * Depth-first traversal of the AST
+             */
             void walk( std::function<void( const token & )> callback ) const override;
-
+            /*
+             * Get the character representation of this token
+             */
             character_type get_character() const override;
-
+            /*
+             * Get the type of this token
+             */
             type get_type() const override;
         };
 
@@ -202,13 +256,21 @@ namespace regex
             explicit concatenation( std::unique_ptr<token> lhs, std::unique_ptr<token> rhs );
             explicit concatenation( const concatenation & ) = delete;
             explicit concatenation( concatenation && ) = delete;
-
+            /*
+             * Output this token and its arguments to the output stream
+             */
             void to_string( typename token::ostream &os ) const override;
-
+            /*
+             * Depth-first traversal of the AST
+             */
             void walk( std::function<void( const token & )> callback ) const override;
-
+            /*
+             * Get the character representation of this token
+             */
             character_type get_character() const override;
-
+            /*
+             * Get the type of this token
+             */
             type get_type() const override;
         };
     } // namespace language
