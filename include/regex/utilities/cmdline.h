10e841777ebb879e77ab2deb469877612136cfe7
laurie-windows <laurieharvey1602@gmail.com>
2023-06-27 15:23:35 +0100

    Better CMake. Configure file with version number

--- include/regex/utilities/cmdline.h
+++ include/regex/utilities/cmdline.h
@@ -67,7 +67,8 @@ namespace regex::cmd
         };
 
       private:
-        std::string name_, error_, description_;
+        std::filesystem::path name_;
+        std::string error_, description_;
         std::map<std::string, std::any> args_;
         std::vector<std::tuple<std::string, type, std::string>> positionals_;
         std::map<std::string, std::tuple<std::string, type, std::string, std::vector<std::any>>> optionals_;
